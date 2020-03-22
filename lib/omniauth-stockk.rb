a46c972ea201d994da1b96e99b0b6ed3b0684517
Earl Balai Jr <earl@kiwi.io>
2020-03-22 12:55:04 +0800

    Initial commit

---
+++ lib/omniauth-stockk.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require 'omniauth-stockk/version'
+require 'omniauth/strategies/stockk'
---
+++ lib/omniauth-strategies/stockk.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module OmniAuth
+  module Strategies
+    class Stockk < OmniAuth::Strategies::OAuth2
+      option :name, 'stockk'
+
+      option :client_options, {
+        site: 'http://localhost:3001'
+      }
+
+      uid { raw_info['id'] }
+
+      info do
+        {
+          email: raw_info['email']
+        }
+      end
+
+      extra do
+        {
+          "raw_info": raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v1/me').parsed
+      end
+    end
+  end
+end
